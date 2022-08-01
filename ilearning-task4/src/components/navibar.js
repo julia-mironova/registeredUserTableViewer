eb2092611caaa361f99b9434da0cf2e6d177034e
Yuliya Mironova <rabkevich15@gmail.com>
2022-08-01 14:16:54 +0200

    fix: navibar

--- ilearning-task4/src/components/navibar.js
+++ ilearning-task4/src/components/navibar.js
@@ -8,25 +8,26 @@ function NaviBar() {
 		<>
 			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-				<Navbar.Brand>ILearning, task4</Navbar.Brand>
-				<Nav className="mr-auto">
-					<Nav.Link>
-						<Link to="/">Home</Link>
-					</Nav.Link>
-					<Nav.Link>
-						<Link to="/users">Users</Link>
-					</Nav.Link>
-					<Nav.Link>
-						<Link to="/register">Register</Link>
-					</Nav.Link>
-				</Nav>
-				<Nav>
-					<Button variant="primary" className="mr-2">
-						Log in
-					</Button>
-					<Button variant="primary">Sign out</Button>
-				</Nav>
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Navbar.Brand>ILearning, task4</Navbar.Brand>
+					<Nav className="mr-auto">
+						<Nav.Link>
+							<Link to="/">Home</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/users">Users</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/register">Register</Link>
+						</Nav.Link>
+					</Nav>
+					<Nav>
+						<Button variant="primary" className="mr-2">
+							Log in
+						</Button>
+						<Button variant="primary">Sign out</Button>
+					</Nav>
+				</Navbar.Collapse>
 			</Navbar>
 		</>
 	);
