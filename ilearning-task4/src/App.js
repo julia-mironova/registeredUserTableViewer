ed7b69a9696c063fef6d1464738cada2a67bfe43
Yuliya Mironova <rabkevich15@gmail.com>
2022-08-01 12:38:52 +0200

    feat: nav

--- ilearning-task4/src/App.js
+++ ilearning-task4/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NaviBar from "./components/navibar";
+import Home from "./pages/home";
+import Users from "./pages/users";
+import RegisterUser from "./pages/register";
+import Footer from "./components/footer";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">table</header>
-		</div>
+		<Router>
+			<div className="App">
+				<NaviBar />
+				<Routes>
+					<Route path="/" component={Home} />
+					<Route path="/users" component={Users} />
+					<Route path="/register" component={RegisterUser} />
+				</Routes>
+				<main className="App-main">main</main>
+				<footer className="App-footer">
+					<Footer />
+				</footer>
+			</div>
+		</Router>
 	);
 }
 
---
+++ ilearning-task4/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
+function Footer() {
+	return (
+		<>
+			<h2>Footer</h2>
+			<Button variant="primary">Github link to this project</Button>
+		</>
+	);
+}
+
+export default Footer;
